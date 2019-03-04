0ca97a7cc01372af0f731823ea5cfc58240d729e
Thales Avila <thalespizza@gmail.com>
2019-03-03 21:45:06 -0300

    Adicionando views

--- routes/admin.js
+++ routes/admin.js
@@ -6,7 +6,7 @@ router.get('/add-product', (req, res) => {
   res.send('Form para add produto');
 });
 
-router.post('/product', (req, res) => {
+router.post('/add-product', (req, res) => {
   console.log(req.body);
   res.redirect('/');
 });
