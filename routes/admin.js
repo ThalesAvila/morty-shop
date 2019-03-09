24626bbf1b2b152713440405d957717e99215257
Thales Avila <thalespizza@gmail.com>
2019-03-09 14:35:29 -0300

    Implementando producsts controller

--- routes/admin.js
+++ routes/admin.js
@@ -1,20 +1,12 @@
+// Dependências
 const express = require('express');
+
+const productsController = require('../controllers/products');
 
 const router = express.Router();
 
-const products = [];
+router.get('/add-product', productsController.getAddProduct);
 
-router.get('/add-product', (req, res) => {
-  res.render('add-product', {
-    pageTitle: 'Add Product',
-    path: '/admin/add-product',
-  });
-});
+router.post('/add-product', productsController.postAddProduct);
 
-router.post('/add-product', (req, res) => {
-  products.push({ title: req.body.title });
-  res.redirect('/');
-});
-
-exports.routes = router;
-exports.products = products;
+module.exports = router;
--- routes/shop.js
+++ routes/shop.js
@@ -2,14 +2,8 @@ const express = require('express');
 
 const router = express.Router();
 
-const { products } = require('./admin');
+const productsController = require('../controllers/products');
 
-router.get('/', (req, res) => {
-  res.render('shop', {
-    prods: products,
-    pageTitle: 'Shop',
-    path: '/',
-  });
-});
+router.get('/', productsController.getProducts);
 
 module.exports = router;
