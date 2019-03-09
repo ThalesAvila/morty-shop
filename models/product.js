b2e5bcaa771f03d0e9090a54ecd3b87921b246f0
Thales Avila <thalespizza@gmail.com>
2019-03-09 18:46:49 -0300

    Implementando metodos do product model

--- models/product.js
+++ models/product.js
@@ -1,4 +1,8 @@
-const products = [];
+const fs = require('fs');
+const path = require('path');
+const root = require('../util/path');
+
+const p = path.join(root, 'data', 'products.json');
 
 module.exports = class Product {
   constructor(title) {
@@ -6,10 +10,25 @@ module.exports = class Product {
   }
 
   save() {
-    products.push(this);
+    fs.readFile(p, (err, fileContent) => {
+      let products = [];
+      if (!err) {
+        products = JSON.parse(fileContent);
+      } else {
+        products.push(this);
+      }
+      fs.writeFile(p, JSON.stringify(products), (err) => {
+        console.log(JSON.stringify(products));
+      });
+    });
   }
 
   static fetchAll() {
-    return products;
+    fs.readFile(p, (err, fileContent) => {
+      if (err) {
+        return [];
+      }
+      return JSON.parse(fileContent);
+    });
   }
 };
